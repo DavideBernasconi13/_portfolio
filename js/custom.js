627fae3db9c0add76e5e1a1493463846df2455f4
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-09-11 16:21:26 +0200

    button go to top

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,19 @@
-console.log("Ciao!"); 
+const mybutton = document.getElementById("goToTop");
+
+// quando la finestra scorre, chiama la funzione
+window.onscroll = function () { scrollFunction() };
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function goToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
