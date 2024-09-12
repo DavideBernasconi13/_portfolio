b457754cb02d0c4385603b54e6b5cebe241a14b8
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-09-12 13:37:15 +0200

    menu-responsive

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,6 @@
+/* -------------------------------------------------------------------------------- */
+/* ! Go to top */
+/* -------------------------------------------------------------------------------- */
 const mybutton = document.getElementById("goToTop");
 
 // quando la finestra scorre, chiama la funzione
@@ -17,3 +20,47 @@ function goToTop() {
     document.documentElement.scrollTop = 0;
 }
 
+
+/* -------------------------------------------------------------------------------- */
+/* ! Responsive menu */
+/* -------------------------------------------------------------------------------- */
+
+let navbar = document.querySelector('.navbar');
+let nav = document.querySelector('.nav-menu');
+navbar.addEventListener("click", function () {
+    nav.classList.toggle('open');
+});
+
+/*
+// funzione per determinare la classe active nel menu 
+window.addEventListener('scroll', function () {
+    // Ottieni tutti i link del menu
+    const menuLinks = document.querySelectorAll('.menu-link');
+
+    // Ottieni tutte le sezioni
+    const sections = document.querySelectorAll('section');
+
+    let currentSection = '';
+
+
+    // Loop attraverso le sezioni per trovare quella visibile
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (window.scrollY >= sectionTop - sectionHeight / 3) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    // Rimuovi la classe "active" da tutti i link
+    menuLinks.forEach(link => {
+        link.classList.remove('active');
+        
+
+        // Aggiungi la classe "active" al link corrispondente alla sezione visibile
+        if (link.getAttribute('href') === '#' + currentSection) {
+            link.classList.add('active');
+        }
+    });
+});
+*/
